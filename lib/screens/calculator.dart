9093e0da1226b787c6ed3ad6f6d57da00177f332
Hiva Auuuuu <thiagomatos.tlm@gmail.com>
2025-02-12 13:12:27 -0300

    First commit: created flutter projected

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('oi'),
+      ),
+    );
+  }
+}
